a30d6effee9e9188c9b08d3e8c948082fe2e7f79
Fernando Ortega <feortegas@gmail.com>
2021-11-08 00:28:32 -0600

    create elements in html for repo data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var reposSearchTerm = document.querySelector("#repo-search-term");
 
 // for submit event handler
 var formSubmitHandler = function(event) {
@@ -34,6 +36,31 @@ var getUserRepos = function(user) {
 var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    reposSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+
 };
 
 
